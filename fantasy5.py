73187b02ffe65e4f7e584b8f14aa77f881ef4634
Stephen Moon <stephen.moon@gmail.com>
2021-02-09 14:58:05 -0800

    Fix a bug in showing usage

--- fantasy5.py
+++ fantasy5.py
@@ -68,7 +68,7 @@ class Fantasy5(object):
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage: {0} winning_numbers.txt [average|chance]")
+        print("Usage: {0} winning_numbers.txt [average|chance]".format(sys.argv[0]))
         sys.exit(1)
 
     filename = sys.argv[1]
